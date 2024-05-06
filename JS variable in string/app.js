86f82ea7c6e893f6871a0b961cb362f27649fb5e
konainbaig78 <konainbaig915@gmail.com>
2024-05-06 08:27:53 +0500

    done

--- JS variable in string/app.js
+++ JS variable in string/app.js
@@ -1,2 +1,17 @@
 // Question # 1
-//
+// var username;
+
+//Question # 2
+// var myName = "Konain  Baig"
+
+//Question # 3
+// var message=" Hello World "
+// alert(message)
+
+//Question # 4
+// var name=" Jhon Doe "
+// var age=" 15 years old "
+// var skill=" Certified Mobile Application Development "
+// alert (name)
+// alert (age)
+// alert (skill)
