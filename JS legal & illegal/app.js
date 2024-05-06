c5a0c72a37378ae73e53f90a0c9fa1abbfb0c7b9
konainbaig78 <konainbaig915@gmail.com>
2024-05-06 08:20:02 +0500

    JavaScript

---
+++ JS legal & illegal/app.js
@@ -0,0 +1,35 @@
+// Question 1
+// var name ;
+// var num1To ;
+// var _myBook ;
+
+// Question 2
+//     LEGAL VARIABLE
+// var myName;
+// var $money;
+// var user_name;
+// var thisAlert;
+// var _prompt_1;
+//      ILLEGAL VARIABLE
+// var 12myNumber;
+// var alert;
+// var var;
+// var %prompt;
+// var user>name;
+
+// Question 3
+// document.write( " <h1><b>Rules for naming JS variable</b></h1> ")
+// document.write()
+// document.write()
+
+// var number=" number "
+// var alpha= " name "
+// var spe= " $ "
+// var un1= " _ "
+// var sens=" sensitive "
+// var key=" keyword "
+
+// document.write(" Variable name can only contain " + number   + alpha   + spe + " and " + un1 ) 
+// document.write(" Variable must begin with " + spe   + un1   + " or " + alpha)
+// document.write( " Variable name are case " + sens )
+// document.write( " Variable name should not bs JS " + key )
---
+++ JS variable in numbers/app.js
@@ -0,0 +1,17 @@
+// Question 1
+// var age="I am 16 years old"
+// alert (age)
+
+// Question 2
+// var number=14
+// alert ("You have visited this site " + number + " times")
+
+// Question 3
+// var birthYear= 2008
+// document.write("My birth year is " + birthYear)
+
+// Question 4
+// var visitorName=" Jhone Doe "
+// var productTitle= " T-shirts"
+// var quantity= 5
+// document.write( visitorName + " ordered " + quantity + productTitle + " on XYZ Clothing Store")
