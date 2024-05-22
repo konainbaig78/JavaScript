677eb9ef8096f6529512314c9257992981a67df5
konainbaig78 <konainbaig915@gmail.com>
2024-05-22 08:34:25 +0500

    :sparkle:

---
+++ JS Alert chp 1/app.js
@@ -0,0 +1,21 @@
+//  Question 1
+//alert ( "Hello!\nWelcome to my website" );
+
+
+//  Question 2
+//alert ( "Error! Please enter a valid password" );
+
+
+//  Question 3
+// alert ( "Welcome to JS Land... \n Happy Coding!");
+
+
+//Question 4
+// alert ("Welcome to JS Land...");
+// alert ("Happy Coding!");
+
+
+//Question 5
+// console.log(alert("Hello... I can run JS through my web browser's console"));
+
+//END
---
+++ JS chp 9-11/app.js
@@ -0,0 +1,43 @@
+// Question 1
+// var cityName=prompt("Enter your city name");
+// if (cityName==="karachi"){
+//     alert("Welcome to the city of lights!");
+// }
+
+
+// Question 2
+// var gender=prompt("Enter gender name");
+// var male="male";
+// var female="female";
+// if (gender === male){
+//     alert("Good Morning Sir")
+// };
+// if (gender === female){
+//     alert("Good Morning Ma'am")
+// };
+
+
+// Question 3
+
+
+// Question 4
+// var fuel=prompt("Remaining fuel in a car in liters.");
+// if (fuel < 0.25){
+//     alert("Please refill the fuel in your car")
+// };
+
+
+// Question 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true")}
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
